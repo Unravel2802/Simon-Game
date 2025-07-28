142dbc627f030c372479a2872eb94d964fb2e7f0
Hoang Quoc Ly <hoanglyquoc@gmail.com>
2025-07-28 22:27:30 +0700

    add next sequence function

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,2 +1,24 @@
+var buttonColours = ["red", "green", "blue", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
 
+function nextSequence() {
+    level ++;         
+
+    $("#level-title").text("Level: " + level);
+
+    var rand = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[rand];
+
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour) 
+        .fadeIn(100)
+        .fadeOut(100)
+        .fadeIn(100);
+
+    playSound(randomChosenColour);
+}
 
